8b56d85996e85b54150d4be821d5e6c1d9fd0c9c
PNU-Seung <ksgg1navercom@gmail.com>
2021-08-27 08:09:13 +0900

    add administior page

--- Server/volumes/api/admin.py
+++ Server/volumes/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Music, User, UserSong, Friend
+
+admin.site.register(Music)
+admin.site.register(User)
+admin.site.register(UserSong)
+admin.site.register(Friend)
